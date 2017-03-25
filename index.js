b338497baad1f43f0bd22994b679530e64802725
AndyIsCool5463 <www.andyiscool@outlook.com>
2017-03-24 20:41:43 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,29 @@ const bot = new commando.Client({
 owner: '201825529333153792',
 commandPrefix: '+'
 });
+bot.on('ready', () => {
+  console.log('I am ready!');
+});
+
+bot.on('message', message => {
+  // if the message is "what is my avatar",
+  if (message.content === 'what is my avatar') {
+    // send the user's avatar URL
+    message.reply(message.author.avatarURL);
+  }
+});
+
+bot.on('message', message => {
+  // if the message is "+invite",
+  if (message.content === '+invite') {
+    // send the invite link 
+    message.reply(" Here it is! https://discord.gg/aDpcxd4 Share with all friends!");
+  }
+});
 
 
 bot.registry.registerGroup('random', 'random');
+bot.registry.registerGroup('misc', 'misc');
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "/commands")
 
